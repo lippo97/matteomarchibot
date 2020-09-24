ed42348cb0fd525e464d9145efdbafd912ca90f8
Lippo <filippo.nardini@studio.unibo.it>
2020-09-24 22:57:03 +0200

    Add support for environment variable LOCALE

--- bot/i18n_conf.py
+++ bot/i18n_conf.py
@@ -1,10 +1,11 @@
 import os
 import i18n as _i18n
 
+current_locale = os.getenv('LOCALE') or 'it'
 locales_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../locales")
 
 _i18n.load_path.append(locales_dir)
-_i18n.set('locale', 'it')
+_i18n.set('locale', current_locale)
 _i18n.set('fallback', 'en')
 _i18n.set('enable_memoization', True)
 
